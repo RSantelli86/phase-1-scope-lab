5f3b4a282d090098c106fe24535e9d1f5d0506ad
Rey <rsantelli@luc.edu>
2024-02-01 12:08:53 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(name) {
+  bestCustomer = name;
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'initial value';
+  leastFavoriteCustomer = 'attempted reassignment'; // This should cause an error
+}
